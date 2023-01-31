37d4185d6980f622469c7cb07554b4a426774d73
tongzhoutz <tzhou11@jhu.edu>
2023-01-31 13:25:00 -0500

    feat: 🎸 add LinearSearch function for int array

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
+import LinearSearch.LinearSearch;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Start off from master");
-    }
-    public static void testInt(int a) {
-        System.out.println(a);
+        System.out.println("Hello World!");
     }
 }
