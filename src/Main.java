d6149479cad2600c097ed7e4d7db64894e66e2c7
tongzhoutz <tzhou11@jhu.edu>
2023-01-31 12:54:40 -0500

    test: 💍 test git on intelliJ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Start off from master");
+    }
+    public static void testInt(int a) {
+        System.out.println(a);
     }
 }
