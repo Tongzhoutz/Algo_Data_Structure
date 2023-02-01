2406b73ac1def6ae6fca25084d829cc2da0f7b34
tongzhoutz <tzhou11@jhu.edu>
2023-02-01 08:43:25 -0500

    feat: 🎸 add Generatics with Comparable interface

--- src/SelectionSort/SelectionSort.java
+++ src/SelectionSort/SelectionSort.java
@@ -5,26 +5,26 @@ import java.util.Arrays;
 public class SelectionSort {
     private SelectionSort() {};
 
-    public static void selectionSort(int[] arr) {
+    public static <E extends Comparable<E>> void selectionSort(E[] arr) {
 
         for (int i = 0; i < arr.length; i++) {
             int minIndex = i;
             for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] < arr[minIndex])
+                if (arr[j].compareTo(arr[minIndex]) < 0)
                     minIndex = j;
             }
             swap(arr, i, minIndex);
         }
     }
 
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
+    public static <E> void swap(E[] arr, int i, int j) {
+        E temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
     public static void main(String[] args) {
-        int[] arr = {6, 4, 2, 3, 1, 5};
+        Integer[] arr = {6, 4, 2, 3, 1, 5};
         selectionSort(arr);
         for (int e : arr)
             System.out.print(e + " ");
